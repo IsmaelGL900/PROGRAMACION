a455b4a21c31b888308d9f3b1081deaa5eb2ef28
IsmaelGL900 <150629024+IsmaelGL900@users.noreply.github.com>
2025-02-03 17:47:27 +0100

    Add files via upload

--- proyectoSupermercado/src/main/java/utilidades/UtilidadesContrato.java
+++ proyectoSupermercado/src/main/java/utilidades/UtilidadesContrato.java
@@ -1,4 +1,49 @@
 package utilidades;
 
+import modelos.Contrato;
+import modelos.TipoContrato;
+
+import java.util.*;
+
 public class UtilidadesContrato {
+
+    public Map<TipoContrato, Double> getSalarioMedioTipoContrato(List<Contrato> contratos) {
+        Map<TipoContrato, Double> salarioMedioTipoContrato = new HashMap<>();
+
+        for (Contrato c : contratos) {
+            TipoContrato tipoContrato = c.getTipoContrato();
+            double salario = c.getSalarioBase();
+            salarioMedioTipoContrato.put(tipoContrato, salario);
+        }
+        System.out.println(salarioMedioTipoContrato);
+        return salarioMedioTipoContrato;
+    }
+
+    public Map<TipoContrato, Integer> getNumContratosPorTipo(List<Contrato> contratos) {
+        Map<TipoContrato, Integer> numContratosPorTipo = new HashMap<>();
+        for (Contrato c : contratos) {
+            TipoContrato tipoContrato = c.getTipoContrato();
+
+            if (numContratosPorTipo.containsKey(tipoContrato)) {
+                numContratosPorTipo.put(tipoContrato, numContratosPorTipo.get(tipoContrato) + 1);
+            } else {
+                numContratosPorTipo.put(tipoContrato, 1);
+            }
+
+        }
+        return numContratosPorTipo;
+    }
+
+    public Map<TipoContrato, List<Contrato>> getListContratosPorTipo(List<Contrato> contratos) {
+        Map<TipoContrato, List<Contrato>> listContratosPorTipo = new HashMap<>();
+
+        for (Contrato c : contratos) {
+            TipoContrato tipoContrato = c.getTipoContrato();
+            listContratosPorTipo.putIfAbsent(tipoContrato, new ArrayList<>());
+            listContratosPorTipo.get(tipoContrato).add(c);
+        }
+        return listContratosPorTipo;
+    }
+
+
 }
